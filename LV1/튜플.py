9492288d2ab72da41eb2f5ae069fb4527850663c
최호준 <67067346+wns6743@users.noreply.github.com>
2022-05-02 20:58:18 +0900

    문제 해결

--- LV1/튜플.py
+++ LV1/튜플.py
@@ -2,16 +2,52 @@
 import collections
 def solution(s):
     dic = {}
-    s = collections.deque(list(s))
-    answer = [] 
-    while s :
-        temp = []
-        if s[0] == "{":
-            while s and  s[0] !="}" :
-                temp.append(s.popleft())
-        if s and s[0] == "}" :
-            s.popleft()
-        # answer.append(temp)
+    count=0
+    temp=[]
+    answer=[]
+    long =''
+    s = collections.deque(s)
+    s.popleft()
+    s.pop()
+    while s:
+        x=s.popleft()
+        if x == "{":
+            while True:
+                x =s.popleft()
+                if x == "}":
+                    temp.append(int(long))
+                    dic[count] = temp
+                    temp=[]
+                    count =0
+                    long =''
+                    break
+                elif x == "," :
+                    temp.append(int(long))
+                    count +=1
+                    long=''
+                
+                
+                else :
+                    long+=x
+        
+        
+    
+    keys =sorted(dic.keys())
+    for key in keys:
+        if key=="0":
+            continue 
+        for k in dic[key] :
+            if int(k) not in answer:
+                answer.append(int(k))
+        
+        
+        
+            
+        
+        
+        
+        
+        
     
 
     return answer
