f57a3362ae236a9cc1ad2034fa4d86d76c085033
최호준 <67067346+wns6743@users.noreply.github.com>
2022-05-05 10:36:07 +0900

    Create 괄호회전.py

---
+++ LV1/괄호회전.py
@@ -0,0 +1,30 @@
+import collections
+def isCorrect(s):
+    stack = []
+    _len = 0
+    for x in s :
+        if  x == "[" or x== "{" or x == "(" :
+            stack.append(x) 
+            _len +=1 
+        elif _len >0 :
+            if stack[-1] == "[" and x == "]":
+                stack.pop()
+                _len -=1
+            elif stack[-1] == "(" and x == ")":
+                stack.pop()
+                _len -=1 
+            elif stack[-1] =="{" and x== "}":
+                stack.pop()
+                _len-=1
+        else :
+            return False
+    return True if _len == 0 else False
+def solution(s):
+    answer = 0
+    q = collections.deque(s)
+    for i in range(len(s)):
+        if isCorrect(q) : 
+            answer+=1 
+        q.append(q.popleft())
+    return answer
+
