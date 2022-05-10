200915e0c8d7e7280910d5ec1972b7333723458e
wns6743 <wns674@naver.com>
2022-05-11 02:52:29 +0900

    키패드 누르기 2번 풀이

--- LV1/키패드누르기.py
+++ LV1/키패드누르기.py
@@ -1,3 +1,6 @@
+from pydoc import locate
+
+
 def solution(numbers, hand):
     answer = []
     phone = [[1,2,3],[4,5,6],[7,8,9],[10,0,11]]
@@ -27,8 +30,35 @@ def solution(numbers, hand):
             elif l_d > r_d :
                  right =append("R",index)
             else :
-                 left = append("L",index)
-                    
-            
-                
+                 left = append("L",index)     
     return ''.join(answer)
+
+def solution(numbers,hand):
+    answer = [] 
+    location = [[3,1],[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2],[2,2]]
+    left = [3,0]
+    right =[3,2]
+    def append(hand,point):
+        answer.append(hand)
+        return location[point]
+
+    for n in numbers:
+        if n %3 == 1 :
+            left= append("L",n)
+        elif n!=0 and  n % 3 == 0  :
+            right =append("R",n)
+        else  : 
+            index = location[n]
+            l_d = abs(left[0]-index[0])+abs(left[1]-index[1])
+            r_d = abs(right[0]-index[0])+abs(right[1]-index[1])
+            if l_d == r_d :
+                if hand == "right": 
+                     right =append("R",n)
+                else :
+                    left =append("L",n)
+            elif l_d > r_d :
+                 right =append("R",n)
+            else :
+                 left = append("L",n)
+    return ''.join(answer)
+           
