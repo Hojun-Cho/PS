c249c31972e228fd6a55a6a24295f2ac709c4c61
wns6743 <wns674@naver.com>
2022-05-13 10:29:53 +0900

    행렬 테두리 회전하

--- LV1/test.py
+++ LV1/test.py
@@ -1,14 +1,40 @@
-from itertools import product
-def solution(word):
-    pattern = ['A','E','I','O','U']
-    answer = 1
-    stack = []
-    for i in range(1,6):
-        for j in product(pattern,repeat =i):
-            stack.append(''.join(j))
+def initArray(rows,columns):
+    size = max(rows,columns)
+    arr =[[0]*(size+1) for _ in range(size+1)] 
+    num = 0
+    for i in  range(1,rows+1) :
+        for j in range(1,columns+1) :
+            num+=1 
+            arr[i][j] = num
+    return arr
+def solution(rows, columns, queries):
+    answer = []
+    arr =  initArray(rows,columns)
+
+    for x_s,y_s ,x_e,y_e  in queries :
+        last=temp = arr[x_s][y_s] 
+        for y in range(y_s+1,y_e+1) :
+            if y <=  rows :
+                arr[x_s][y],last= last,arr[x_s][y]
+                temp = min(temp,last)
+        for x in range(x_s+1,x_e+1) :
+            if x <= columns :
+                arr[x][y_e] ,last = last , arr[x][y_e] 
+                temp = min(temp,last)
+        for y in range(y_e-1,y_s-1,-1):
+            if y > 0 :
+                arr[x_e][y] ,last = last,arr[x_e][y] 
+                temp = min(temp,last)
+        for x in range(x_e-1,x_s-1,-1):
+            if x > 0 : 
+                arr[x][y_s],last = last,arr[x][y_s] 
+                temp = min(temp,last) 
             
-    stack.sort();
-    return stack.index(word)+1 
-    
-        
-solution("AAAE");
+        answer.append(temp)       
+    return answer 
+                    
+
+                    
+# solution(6,6        ,[[2,2,5,4],[3,3,6,6],[5,1,6,3]])
+# solution(3,	3,	[[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]])
+solution(100,	97	,[[1,1,100,97]])
