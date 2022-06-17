20860e93cbf21f437f9d0388ba6de6d5263fe716
최호준 <67067346+wns6743@users.noreply.github.com>
2022-06-17 18:03:15 +0900

    Update n개의최소공배수.py
    풀이2

--- LV1/n개의최소공배수.py
+++ LV1/n개의최소공배수.py
@@ -1,3 +1,10 @@
+import math
+def solution(arr):
+    answer = arr[0]
+    for num in arr:
+        answer = answer* num // math.gcd(answer,num)
+    return answer
+
 def solution(arr):
     arr.sort()
     answer = arr[0]
