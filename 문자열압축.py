5232bef996350cad8631df3a9bef64da150d7437
최호준 <67067346+wns6743@users.noreply.github.com>
2022-06-25 00:21:13 +0900

    Create 문자열압축.py

---
+++ 문자열압축.py
@@ -0,0 +1,23 @@
+# deque가 아닌 다른 풀이로도 꼭 풀어보자 
+import collections 
+def solution(s):
+    answer  ,length = 1001,len(s)
+    if length == 1 :
+        return 1 
+    for i in range(1,length//2+1)  :
+        result = collections.deque([ s[j:j+i] for j in range(0,length,i)])
+        tempAnswer = ''
+        last = result[0]
+        rept = 0
+        while result :
+            now  = result.popleft()
+            if now == last : 
+                rept +=1
+            elif now != last :
+                tempAnswer += (str(rept)+last) if rept > 1 else last 
+                rept = 1 
+                last = now      
+            if not result : 
+                tempAnswer += (str(rept) + last) if rept > 1 else last
+        answer = min(len(tempAnswer),answer)
+    return answer
