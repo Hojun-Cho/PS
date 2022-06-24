e715ef9acf812e9741d45fa58a8712ffabab593f
최호준 <67067346+wns6743@users.noreply.github.com>
2022-06-25 00:33:12 +0900

    Update 문자열압축.py

--- 문자열압축.py
+++ 문자열압축.py
@@ -21,3 +21,24 @@ def solution(s):
                 tempAnswer += (str(rept) + last) if rept > 1 else last
         answer = min(len(tempAnswer),answer)
     return answer
+
+
+### 소소한 재미. . .
+import collections 
+def solution(s):
+    answer  ,length = 1001,len(s)
+    if length == 1 : return 1 
+    results = []
+    [results.append( collections.deque([ s[j:j+i] for j in range(0,length,i)])) for i in range(1,length//2 +1)]
+    for result in results :
+        last ,rept ,tempAnswer= result[0] , 0 , ''
+        while result :
+            now  = result.popleft()
+            rept +=1 if now == last else 0
+            if now != last :
+                tempAnswer += (str(rept)+last) if rept > 1 else last 
+                rept = 1 
+                last = now      
+            if not result : tempAnswer += (str(rept) + last) if rept > 1 else last
+        answer = min(len(tempAnswer),answer)
+    return answer
