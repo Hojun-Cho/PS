bc9eee67d7fc35e2b948fd21618e3dc590bf37a3
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-06-28 23:50:06 +0900

    Update 다단계칫솔.py

--- LV1/다단계칫솔.py
+++ LV1/다단계칫솔.py
@@ -1,4 +1,38 @@
-# 자신보다 위에 있는 아이들을 배열에 넣는다
+## 처음 풀이 
+def solution(enroll, referral, seller, amount):
+    answer={}
+    referrals = {}
+    def calculate(now_money,now_people):
+        if referrals[now_people] == "-" :
+            if now_money * 0.1 < 1 :
+                answer[now_people] += now_money 
+            else :
+                now_money -= int(now_money*0.1) 
+                answer[now_people] += now_money 
+            return 0
+        if now_money * 0.1 < 1 :
+            answer[now_people] += now_money 
+            return 0
+        next_money = int(now_money*0.1)
+        answer[now_people] += (now_money - next_money)
+        return next_money
+    for i in range(len(enroll)) :
+        answer[enroll[i]] = 0
+        referrals[enroll[i]] = referral[i]
+
+    for i in range(len(seller)) : 
+        people,count = seller[i] , amount[i] 
+        now_people ,now_money = people , count *100 
+        while True :
+            result = calculate(now_money,now_people)
+            if result == 0 :
+                break 
+            now_people,now_money = referrals[now_people],result
+
+
+    return list(answer.values())
+
+## 다음 풀이 
 def solution(enroll, referral, seller, amount):
     answer,peoples = {} ,{}
     def calculate(now_money,now_people):
@@ -11,9 +45,11 @@ def solution(enroll, referral, seller, amount):
             return 0
         answer[now_people] += (now_money - int(referral))
         return int(referral)
+    
     for i in range(len(enroll)) :
         answer[enroll[i]] = 0
         peoples[enroll[i]] = referral[i]
+        
     for now_people,count in zip(seller,amount): 
         now_money = count *100 
         while True :
