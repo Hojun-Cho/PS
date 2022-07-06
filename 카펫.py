9774c2277fab66d6aeec3b29366c0b0026050636
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-07-07 00:03:11 +0900

    Update 카펫.py

--- 카펫.py
+++ 카펫.py
@@ -1,6 +1,6 @@
 def solution(brown, yellow):
     target = brown + yellow 
-    for i in range(3,target//2 +1 ):
+    for i in range(3,int(target**0.5) +1 ):
         x = i  # 세로            3         4   6 
         y = target / i # 가로    8        12    8  
         if  x >y :
