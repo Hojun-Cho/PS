6a4b14b314e3dfd4dc861c74d2dd4b7e1b14c70a
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-07-06 23:58:32 +0900

    Create 카펫.py

---
+++ 카펫.py
@@ -0,0 +1,11 @@
+def solution(brown, yellow):
+    target = brown + yellow 
+    for i in range(3,target//2 +1 ):
+        x = i  # 세로            3         4   6 
+        y = target / i # 가로    8        12    8  
+        if  x >y :
+            continue
+        if (x-2) * (y-2) == yellow :
+            if x * y  == target :
+                return [y,x] 
+        
