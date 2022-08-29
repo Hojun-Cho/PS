b4b883de29b86ec628f625c5b0c0798e0ff6bed3
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-08-29 23:58:02 +0900

    Update 6_zigzag_conversion.go

--- leetCode/6_zigzag_conversion.go
+++ leetCode/6_zigzag_conversion.go
@@ -19,3 +19,33 @@ func convert(s string, numRows int) string {
 	}
 	return builder.String()
 }
+
+// map 없이
+func convert(s string, numRows int) string {
+    // numRows == 1 처리를 안하면 36라인에서 panic => zzLen이 0 이라서
+    if numRows == 1 {
+        return s
+    }
+    len := len(s)
+	zzLen := numRows*2 - 2
+	buf := strings.Builder{}
+	for i := 0; i < numRows; i++ {
+		temp := i
+		if i == 0 || i == numRows-1 {
+			for temp < len {
+				buf.WriteByte(s[temp])
+				temp += zzLen
+			}
+		} else {
+			for temp < len {
+				buf.WriteByte(s[temp])
+				next := temp + zzLen - 2*i
+				if next < len {
+					buf.WriteByte(s[next])
+				}
+				temp += zzLen
+			}
+		}
+	}
+	return buf.String()
+}
