3c56990d875eb0ebcfb5bbcbfe0a9995ba53d74e
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-03 00:13:47 +0900

    Update Sort_Colors.py

--- leetCode/Sort_Colors.py
+++ leetCode/Sort_Colors.py
@@ -26,22 +26,23 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         self.sort(nums,0,len(nums)-1) 
       
-# 규칙이 존재 
+# red index : white의 첫 번째 항목을 가르킨다, 즉 항상 white의 첫 번째 요소 나타낸다
+# white index : 현재 탐색중인 요소
+# blue index : 자신의 인덱스 이후의 배열은 정렬된 상태임을 나타낸다
+# white index가 red 이면 white index의 위치가 white의 첫 번째가 아니므로 red index를 +1, 그리고 다음 요소를 탐색하기 위해서 white index +1
+# white index가 blue이면 white index의 값과 blue index의 값을 교체한다. 그리고 blue inde 에 -1을 더해주면 blue index 뒤까지는 모두 정렬된 상태가 된다  
+# 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        # r,w는 index 0 부터 시작, blue는 index 거꾸로 시작
         red,white,blue = 0,0,len(nums)-1
-        # white 가 blue 까지 도달해야 정렬이 되었다고 할 수 있다. 
         while white <= blue : 
             if nums[white] == 0 :
                 nums[red],nums[white] = nums[white],nums[red]
                 red , white = red+1,white+1
             elif nums[white] == 1 :
                 white += 1
-            # nums[white] == blue
             else :
                 nums[white],nums[blue] = nums[blue],nums[white]
-                # nums[blue] 에서 온 값이 무엇인지 모르기 때문에 blue만 -1
                 blue-=1
 
 
