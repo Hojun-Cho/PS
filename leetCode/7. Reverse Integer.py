b09178b50df9dbdc03a93108b21f306825dc5c58
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-05 19:15:31 +0900

    Update 7. Reverse Integer.py

--- leetCode/7. Reverse Integer.py
+++ leetCode/7. Reverse Integer.py
@@ -16,3 +16,16 @@ class Solution:
             return 0 
         # 원래 부호를 곱한다
         return answer
+    
+class Solution:
+    def reverse(self, x: int) -> int:
+        answer = 0 
+        # x의 부호를 저장 , x에 abs를 취해야 하는데 만약 x < 0 이면 (x % 10) 의도하지 않은 값이 나오기 때문에 abs로 양수로 만든다
+        sign = 1 if x > 0 else -1
+        x *= sign
+        while x:
+            answer = answer*10 + x % 10 
+            x //= 10
+        if answer  > (2**31) -1 :
+            return 0 
+        return answer * sign 
