bdf131ee889a4acf08fbdc07d5ca12824a8220f4
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-05 19:39:08 +0900

    Update 13.Roman to Integer.py

--- leetCode/13.Roman to Integer.py
+++ leetCode/13.Roman to Integer.py
@@ -12,3 +12,14 @@ class Solution:
                 answer += symbols[s[i]]
                 i+=1 
         return answer 
+    
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols: dict[str,int] = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        answer = 0
+        s = s.replace("IV","IIII").replace("IX","VIIII")
+        s = s.replace("XL","XXXX").replace("XC","LXXXX")
+        s = s.replace("CD","CCCC").replace("CM","DCCCC")
+        for symbol in s :
+            answer += symbols[symbol]
+        return answer 
