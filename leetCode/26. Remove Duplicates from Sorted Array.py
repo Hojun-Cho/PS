c27d48101cbb376f5ebda4b71a112e48a0fe0f33
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-06 01:34:22 +0900

    Create 26. Remove Duplicates from Sorted Array.py

---
+++ leetCode/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        length = len(nums)
+        filled,next  = 0,0
+        while next < length:
+            while next < length-1 and  nums[next] == nums[next+1]:
+                next+=1
+            nums[filled]= nums[next]
+            filled+=1 
+            next+=1
+        return filled
+      
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # 0번 인덱스에는 값이 이미 차있다고 가정하자
+        filled,next = 0,0 
+        # j는 항상 i보다 크다 
+        while next < len(nums):
+            # 중복을 건너뛴다
+            if nums[filled] == nums[next]:
+                next+=1
+            # 현재 next에는 중복되지 않은 값이 들어있다.
+            # i+1에 값을 넣는 이유는 i는 이미 값이 filled
+            else :
+                nums[filled+1] = nums[next]
+                filled+=1 
+        return filled + 1 
