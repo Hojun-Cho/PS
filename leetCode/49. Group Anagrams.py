8ee04502eb8fd4ec5a771f2cacf07f5e853ccecf
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-07 14:50:40 +0900

    Update 49. Group Anagrams.py

--- leetCode/49. Group Anagrams.py
+++ leetCode/49. Group Anagrams.py
@@ -8,7 +8,19 @@ class Solution:
             else :
                 m[sorted_str] = [str]
         return m.values() 
-      
+    
+# for loop에서 연산을 수행하는 것보단 for loop 밖에서 전처리를 미리 해주자    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        keys: List[str]= ["".join(sorted(str)) for str in strs]
+        m: dict[str,List[str]] = {}
+        for str,key in zip(strs,keys):
+            if m.get(key) :
+                m[key].append(str)
+            else :
+                m[key] = [str]
+        return m.values() 
+    
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         m = defaultdict(list)
