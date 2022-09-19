c8d2487936971415d0fa366360bb8e97e9dc45e7
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-19 15:15:23 +0900

    Update 문자열압축.py

--- 문자열압축.py
+++ 문자열압축.py
@@ -1,44 +1,29 @@
-# deque가 아닌 다른 풀이로도 꼭 풀어보자 
-import collections 
-def solution(s):
-    answer  ,length = 1001,len(s)
-    if length == 1 :
-        return 1 
-    for i in range(1,length//2+1)  :
-        result = collections.deque([ s[j:j+i] for j in range(0,length,i)])
-        tempAnswer = ''
-        last = result[0]
-        rept = 0
-        while result :
-            now  = result.popleft()
-            if now == last : 
-                rept +=1
-            elif now != last :
-                tempAnswer += (str(rept)+last) if rept > 1 else last 
-                rept = 1 
-                last = now      
-            if not result : 
-                tempAnswer += (str(rept) + last) if rept > 1 else last
-        answer = min(len(tempAnswer),answer)
-    return answer
+import sys
 
-
-### 소소한 재미. . .
-import collections 
 def solution(s):
-    answer  ,length = 1001,len(s)
-    if length == 1 : return 1 
-    results = []
-    [results.append( collections.deque([ s[j:j+i] for j in range(0,length,i)])) for i in range(1,length//2 +1)]
-    for result in results :
-        last ,rept ,tempAnswer= result[0] , 0 , ''
-        while result :
-            now  = result.popleft()
-            rept +=1 if now == last else 0
-            if now != last :
-                tempAnswer += (str(rept)+last) if rept > 1 else last 
-                rept = 1 
-                last = now      
-            if not result : tempAnswer += (str(rept) + last) if rept > 1 else last
-        answer = min(len(tempAnswer),answer)
+    answer = sys.maxsize
+    length = len(s)
+    for i in range(1,length+1):
+        answer = min(answer,zip(s,length,i))
     return answer
+# count 단위로 압축
+def zip(s: str,length: int,size: int) -> int:
+    last = s[:size]
+    i = 0 
+    count = 0 
+    answer= 0 
+    for i in range(0,length +size ,size):
+        now = s[i:i+size] 
+        if last == now:
+            count +=1                 
+        else :
+            if count > 1 :
+                answer += len(str(count)) + len(last)
+            else :
+                answer += len(last)
+            count = 1 
+            last = now 
+    return answer 
+    
+
+                
