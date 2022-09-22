11b3351d2754a065727dca9173bf86f7afb96bf7
최호준 <67067346+Hojun-Cho@users.noreply.github.com>
2022-09-22 23:07:23 +0900

    Create 숫자문자열과영단어.py

---
+++ 숫자문자열과영단어.py
@@ -0,0 +1,7 @@
+def solution(s):
+    dict = { "1":"one","2":"two","3":"three","4":"four","5":"five","6":"six",
+    "7":"seven","8":"eight","9":"nine","0":"zero"}
+    for key,val in dict.items():
+        s= s.replace(val,key)
+       
+    return int(s) 
